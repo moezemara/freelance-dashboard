535cb3413009463f8cc00fec8cbe160b6e86b8f5
KareemElozeiri <K.elozeiri@gmail.com>
2022-05-06 01:08:22 +0200

    initial homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar  from './Navbar';
+import Home from './Home';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Home/>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+         <nav className="navbar">
+            <h2>Freelanco</h2>
+            <div className="links">
+                <a href="/login">Log In</a>
+                <a href="/signup">Sign Up</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
