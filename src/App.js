61e2e5421ea85b225e963fa3a2076b6831fc48f8
KareemElozeiri <K.elozeiri@gmail.com>
2022-05-06 02:33:45 +0200

    added login and signup forms

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import Navbar  from './Navbar';
 import Home from './Home';
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import SignUp from './SignUp';
+import Login from './Login';
 
 function App() {
     return (
       <Router>
         <div className="App">
           <Navbar/>
-          <Home/>
+          <div className='content'>
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route exact path="/signup">
+                <SignUp/>
+              </Route>
+              <Route exact path="/login"> 
+                <Login/>
+              </Route> 
+            
+            </Switch>
+          </div>
         </div>
       </Router>
     );
---
+++ src/BasicNavbar.js
@@ -0,0 +1,9 @@
+const BasicNavbar = () => {
+    return (    
+         <nav className="navbar">
+            <h2>Freelanco</h2>
+        </nav>
+    );
+}
+ 
+export default BasicNavbar;
