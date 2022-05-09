46808f4ea2b1b0b176096af3c7eec3ddb012cefa
Mohamed Ezzat <moezemara@gmail.com>
2022-05-09 23:26:10 +0200

    test login

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,27 @@
 import axios from "axios";
 const Login = () => {
 
-    const handleLogin = ()=>{
-        fetch("https://").then()
+    const handleLogin = async ()=>{
+
+        data = {
+            username: document.getElementsByName('username').values,
+            password: document.getElementsByName('passowrd').values,
+            'g-recaptcha-response': document.getElementsByName('g-recaptcha-response').values
+        }
+
+        const response = await axios.post('https://api.plebits.com/v1/user/login', data)
+
+        console.log(response);
     }
 
     return (  
         <div className="login">
             <h2>Login</h2>
             <form>
-                <input type="text" placeholder="Username"/>
-                <input type="text" placeholder="Password"/>
-                <button>Login</button>
+                <input type="text" name="username" placeholder="Username"/>
+                <input type="text" name="password" placeholder="Password"/>
+                <input type="text" name="g-recaptcha-response" placeholder="Password" value="bypass"/>
+                <button onclick={handleLogin} >Login</button>
             </form>
         </div>
     );
