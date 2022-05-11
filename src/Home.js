797a1bc1020f1731af57d314fd8c2f122d0b7493
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-05-11 02:43:52 +0200

    USE src/src-images to add images used in src folder

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
+import img1 from './src-images/logo1.jpg'
+import img2 from './src-images/logo2.jpg'
+import img3 from './src-images/logo3.jpg'
+import img4 from './src-images/logo4.png'
+
 const Home = () => {
     return (  
         <div className="home">
-
 
         <div id="home-img">
             <p> <br /><br /></p>
@@ -14,7 +18,17 @@ const Home = () => {
 <button id="home-btn-2">Earn money now</button>
 
         </div>
-            
+
+
+<div id="trusted-by">
+<p>Trusted by</p>
+<img src={img1}  height="90" alt="xnx" />
+<img src={img2}  height="90" alt="xnx" />
+<img src={img3}  height="90" alt="xnx" />
+<img src={img4}  height="90" alt="xnx" />
+
+</div>
+            <p>x</p>
 
         </div>
 
