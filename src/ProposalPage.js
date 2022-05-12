2a4dc8a8181dc4ddae982853ce98233649e1615b
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-05-12 15:11:01 +0200

    updated proposalpage and some styles

--- src/ProposalPage.js
+++ src/ProposalPage.js
@@ -1,47 +1,78 @@
+var totalMoney = 0;
+
 const ProposalPage = () => {
-    return ( 
-<div>
-        <div className="proposalpage">
-            <div></div>
-            <h2>here is a card to preview the job</h2>   
-        </div>
+  return (
+    <div>
+      <div className="proposalpage">
+        <div></div>
+        <h2>here is a card to preview the job</h2>
+      </div>
 
-        <div className="proposalpage">
-            <div></div>
-            <h2>here is a card to view job activity</h2>   
-        </div>
+      <div className="proposalpage">
+        <div></div>
+        <h2>here is a card to view job activity</h2>
+      </div>
 
-        <div className="proposalpage">
-            <div></div>
-            <h1>Proposal</h1>
-            <hr />
+      <div className="proposalpage">
+        <div></div>
+        <h2>Proposal</h2>
+        <hr />
 
-            <label>Cover Letter<br/></label>
-                <div>
-                    <textarea placeholder="write your cover letter here"
-                    rows = "5" cols = "100"></textarea>
-                </div>
-                
-<hr />
-            <label>Drag or upload project files </label>
-            <div className="proposal-attachments">
+        <form action="">
+          <label>
+            Cover Letter
+            <br />
+          </label>
+          <div>
+            <textarea
+              placeholder="write your cover letter here"
+              rows="5"
+              cols="100"
+            ></textarea>
+          </div>
+
+          <hr />
+          <label>Drag or upload project files </label>
+          <div className="proposal-attachments">
             <input type="file"></input>
-            </div>
+          </div>
+          <hr />
+          <label>
+            Add due data and proposed money <br />
+            <br />
+          </label>
+          <div className="proposal-details">
+            <input type="date" placeholder="Due Date" />
+            <input type="text" placeholder="Money $" />
+          </div>
+<hr />
 
-            <div className="proposal-details">
-                    <input type="text" style={{marginRight:10}} placeholder="First name"/>
-                    <input type="text" placeholder="Last name"/>
-                </div>
-        
 
+<div className="price-card">
+    <tr>
+    <td>total price</td>
+    <td>{totalMoney}</td>
+    </tr>
 
+    <tr>
+    <td>FreeLanco fees</td>
+    <td>{totalMoney*0.2}</td>
+    </tr>
 
-            <button>Sign Up</button>
-        </div>
+    <tr>
+    <td>your income</td>
+    <td>{totalMoney*0.8}</td>
+    </tr>
+    
 </div>
-        
 
-     );
-}
- 
+
+
+          <button>submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default ProposalPage;
