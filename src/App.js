81a91933a159489b2f408e0e76cbb6e512abdd79
Mohamed Ezzat <k.elozeiri@gmail.com>
2022-05-18 22:09:33 +0000

    test

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
             <Route exact path="/">
               <Navbar/>
               <Home />
+              
             </Route>
             <Route exact path="/signup">
               <Navbar />
