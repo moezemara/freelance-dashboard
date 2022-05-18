1bb8cd276f81fdb264b3040183b0e18faa6126ce
Mohamed Ezzat <k.elozeiri@gmail.com>
2022-05-18 23:01:30 +0000

    linked profile main page with the api

--- src/FreelancerNavbar.js
+++ src/FreelancerNavbar.js
@@ -1,11 +1,20 @@
+import axios from "axios";
+
 const FreelancerNavbar = () => {
+
+    async function handleLogOut(){
+        console.log(process.env.BASE_API_URL)
+        const response = await axios.post('user/logout').then((res)=>{window.location = '/';});
+        console.log(response);
+    }
+
     return (
         <nav className="navbar">
             <a href="/profile"><h2>Freelanco</h2></a>
             <a href="/browsejobs">Find work</a>
             <div className="links">
                 <a href="/profilesettings">Settings</a>
-                <a href="/logout">Log Out</a>
+                <a onClick={handleLogOut}>Log Out</a>
             </div>
         </nav>
     );
