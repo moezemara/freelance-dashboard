1267a0bfbd826e6304b0d29f704110a2531e7a5c
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-05-21 00:28:46 +0200

    update

--- src/BrowseJobs.js
+++ src/BrowseJobs.js
@@ -1,10 +1,13 @@
+import { useEffect, useState } from "react";
+import axios from "./axios.js"
+import Cookies from 'universal-cookie';
 import ProfileSidebar from "./ProfileSidebar";
 import JobCard from "./JobCard";
 
 
 
 
-jobs_list =
+var jobs_list =
 [
     {title:"Job Card Example", price:"1800", skills:"api, dashboard, web dev",category:"web",
     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem fugiat molestias iste corrupti nemo modi consequatur consectetur accusantium praesentium laboriosam nostrum, assumenda quaerat quas at expedita, quos cumque repellendus pariatur deleniti. Incidunt similique consequatur, modi tempora numquam, dolorem soluta porro at magni illo dignissimos repellendus amet pariatur! Minus, quod quibusdam!",
@@ -30,7 +33,7 @@ const BrowseJobs = () => {
                 window.location = '/';
         }},[]);
         
-    },[accountType]);
+    },[]);
 
 
     const getJobsList = (jobs)=>{
