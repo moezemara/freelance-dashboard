33f08d1f5c254a66f2915195d66b47c9e3d6d9da
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-05-21 00:20:19 +0200

    Browse jobs - mainly finished

--- src/BrowseJobs.js
+++ src/BrowseJobs.js
@@ -3,7 +3,9 @@ import JobCard from "./JobCard";
 
 
 
-var tempJobs = [
+
+jobs_list =
+[
     {title:"Job Card Example", price:"1800", skills:"api, dashboard, web dev",category:"web",
     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem fugiat molestias iste corrupti nemo modi consequatur consectetur accusantium praesentium laboriosam nostrum, assumenda quaerat quas at expedita, quos cumque repellendus pariatur deleniti. Incidunt similique consequatur, modi tempora numquam, dolorem soluta porro at magni illo dignissimos repellendus amet pariatur! Minus, quod quibusdam!",
     attatchment:"https://www.zewailcity.edu.eg/media-library/PDFs/Academic_Calendar_2021-2022__2022-02-16052857.pdf"},
@@ -11,17 +13,32 @@ var tempJobs = [
     {title:"Job Card Example", price:"1800", skills:"api, dashboard, web dev",category:"web",
     description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem fugiat molestias iste corrupti nemo modi consequatur consectetur accusantium praesentium laboriosam nostrum, assumenda quaerat quas at expedita, quos cumque repellendus pariatur deleniti. Incidunt similique consequatur, modi tempora numquam, dolorem soluta porro at magni illo dignissimos repellendus amet pariatur! Minus, quod quibusdam!",
     attatchment:"https://www.zewailcity.edu.eg/media-library/PDFs/Academic_Calendar_2021-2022__2022-02-16052857.pdf"},
-    
 ]
 
 const BrowseJobs = () => {
+
+    const [jobs,setJobs] = useState([]);
+
+    useEffect(()=>{
+        
+        axios.get('freelancer/browsejobs/',{ withCredentials: true}).then(res=>{
+            if(res.data.success===1){
+                setJobs(res.data.message.jobs_list);
+                console.log(res);
+            }
+            else{
+                window.location = '/';
+        }},[]);
+        
+    },[accountType]);
+
 
     const getJobsList = (jobs)=>{
         return (  
             jobs.map((job)=>(<JobCard job={job}/>))
      );}
 
-    var jobsContent = getJobsList(tempJobs);
+    var jobsContent = getJobsList(jobs_list);
 
     return (  
         <div className="browsejobs">
