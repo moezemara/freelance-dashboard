8651c8d8408b195f7ab2c5f5abb89e903ccce8fe
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-05-21 19:00:52 +0200

    home page - loading

--- src/NavbarWide.js
+++ src/NavbarWide.js
@@ -1,14 +1,24 @@
+import logo from "./src-images/homie-logo-1.png";
 
 const NavbarWide = () => {
-    return (  
-         <nav className="navbarwide">
-            <a href="/"><h2>HomieLancer</h2></a>
-            <div className="links">
-                <a href="/login">Log In</a>
-                <a href="/signup">Sign Up</a>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbarwide">
+      <a href="/">
+        <table id="table-of-home-logo">
+          <td>
+            <img src={logo} height="70px" style={{marginTop:"10px"}} />
+          </td>
+          <td>
+            <h2>HomieLancer</h2>
+          </td>
+        </table>
+      </a>
+      <div className="links">
+        <a href="/login">Log In</a>
+        <a href="/signup">Sign Up</a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavbarWide;
