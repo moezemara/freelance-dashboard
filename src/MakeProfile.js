77024d4546164e703034c7deea7e16c504645a9f
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-05-21 04:45:51 +0200

    create profile api done

--- src/MakeProfile.js
+++ src/MakeProfile.js
@@ -1,55 +1,103 @@
-import FreelancerNavbar from './FreelancerNavbar.js';
-import {getCategoriesList, getGenderList} from './Options.js'
+import FreelancerNavbar from "./FreelancerNavbar.js";
+import { getCategoriesList, getGenderList } from "./Options.js";
+import { useState, useEffect, createRef } from "react";
+import axios from "./axios.js";
 
 const MakeProfile = () => {
-    return (  
-        <div>
-        <FreelancerNavbar/>
-        <div className="profilesettings"> {/*let its name profilesettings as changing its name make a lot of trouble in styling*/}
-            <h1>Make your profile</h1>
-            <form>
-                <div>
-                    <input type="text" placeholder="Job Title"/>
-                </div>
-                <div className="name_part">
-                    <select>
-                        <option value=""   disabled selected hidden>Choose Category</option>
-                        {getCategoriesList()}
-                    </select>
-                    <input style={{marginLeft:20}} type="text" placeholder="Pay rate($/hr)"/>
-                </div>
-                <div>
-                    <input type="text" placeholder="Description"/>
-                </div>
-                <div>
-                    <input type="text" placeholder="Skills"/>
-                </div>
-                <div>
-                    <input type="text" placeholder="Pay rate($/hr)"/>
-                </div>
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [skills, setSkills] = useState("");
+  const [description, setDescription] = useState("");
+  const [pay_rate, setPayRate] = useState("");
 
-                <div>
-                <input placeholder="address"/>
-                </div>
-<div className="name_part">
-<input type="text" style={{marginRight:10}} placeholder="phone"/>
+  async function handleCreateProfile() {
+    console.log(process.env.BASE_API_URL);
+    const data = {
+      title: title,
+      category: category,
+      skills: skills.split(","),
+      description: description,
+      pay_rate: pay_rate,
+    };
 
-<select>
-<option value="" disabled selected hidden>Gender</option>                        
-{getGenderList()}
-</select>
-</div>
-                
-                <div className="profile-attachmenents">
+    const response = await axios.post("freelancer/profile", data, {
+      withCredentials: true,
+    });
+
+    setJobMessage(response.data.message);
+
+    if (response.data.success) {
+      console.log(response);
+      document.cookie = JSON.stringify({ type: response.data.message.type });
+    } else {
+      console.log("failed");
+    }
+  }
+
+  return (
+    <div>
+      <FreelancerNavbar />
+      <div className="profilesettings">
+        {" "}
+        {/*let its name profilesettings as changing its name make a lot of trouble in styling*/}
+        <h1>Make your profile</h1>
+        <form>
+          <div>
+            <input
+              type="text"
+              placeholder="Job Title"
+              value={title}
+              onInput={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="name_part">
+            <select
+              value={category}
+              onInput={(e) => setCategory(e.target.value)}
+            >
+              <option value="" disabled selected hidden>
+                Choose Category
+              </option>
+              {getCategoriesList()}
+            </select>
+            <input
+              style={{ marginLeft: 20 }}
+              type="text"
+              placeholder="Pay rate($/hr)"
+              value={pay_rate}
+              onInput={(e) => setPayRate(e.target.value)}
+            />
+          </div>
+          <div>
+            <textarea
+              id="description-make-profile"
+              type="text"
+              style={{ height: "100px" }}
+              placeholder="Description"
+              value={description}
+              onInput={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Skills"
+              value={skills}
+              onInput={(e) => setSkills(e.target.value)}
+            />
+          </div>
+
+          {/* <div className="profile-attachmenents">
                     <input type="file"/>
-                </div>                
-                <button>make profile</button>
+                </div>                 */}
 
-            </form>
+          <button type="button" onClick={handleCreateProfile}>
+            Create profile
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-        </div>
-    );
-}
- 
 export default MakeProfile;
