a8cc6aaddcdecaf1bb3e8e7f0fa85d49a5966269
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-19 18:50:15 +0200

    Update AccountStats.js

--- src/stats/AccountStats.js
+++ src/stats/AccountStats.js
@@ -3,20 +3,28 @@ import axios from "../axios.js";
 
 const AccountStats = (props) => {
 
-    
+    var userNum = props.userNum
+    var freelancerPercent = props.freelancerPercent
+    var clientPercent = props.clientPercent
+    var avgClientRating = props.avgClientRating
+    var avgFreelancerRating = props.avgFreelancerRating
+    var malePercent = props.malePercent
+    var femalePercent = props.femalePercent
+    var bannedNum = props.bannedNum
+
 
 return(
 
 <div className="account-stats">
 <h2>Accounts stats</h2>
-    <p>number of users: <b>xx</b></p>
-    <p>percent of freelancers: <b>xx</b></p>
-    <p>percent of clients: <b>xx</b></p>
-    <p>Avg. clients rating: <b>xx</b></p>
-    <p>Avg. freelancers rating: <b>xx</b></p>
-    <p>Percent of Male users: <b>xx</b></p>
-    <p>Percent of Female users: <b>xx</b></p>
-    <p>Num of banned accounts: <b>xx</b></p>
+    <p>number of users: <b>{userNum}</b></p>
+    <p>percent of freelancers: <b>{freelancerPercent}</b></p>
+    <p>percent of clients: <b>{clientPercent}</b></p>
+    <p>Avg. clients rating: <b>{avgClientRating}</b></p>
+    <p>Avg. freelancers rating: <b>{avgFreelancerRating}</b></p>
+    <p>Percent of Male users: <b>{malePercent}</b></p>
+    <p>Percent of Female users: <b>{femalePercent}</b></p>
+    <p>Num of banned accounts: <b>{bannedNum}</b></p>
 
 </div>
 
