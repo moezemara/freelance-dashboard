59e10f2dbda09b209829e77c42a4b3fb82ab2e7f
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-19 15:15:19 +0200

    Create ContractStats.js

---
+++ src/stats/ContractStats.js
@@ -0,0 +1,27 @@
+import { useState, useEffect, createRef } from "react";
+import axios from "../axios.js";
+
+const ContractStats = (props) => {
+
+    
+
+return(
+
+
+<div className="contract-stats">
+
+<h2>Contracts stats</h2>
+
+    <p>In-intervieiw contracts: <b>xx</b></p>
+    <p>Active contracts: <b>xx</b></p>
+    <p>Archieved contracts: <b>xx</b></p>
+    <p>Avg. Final price: <b>xx</b></p>
+
+</div>
+
+
+);
+
+}
+
+export default ContractStats
