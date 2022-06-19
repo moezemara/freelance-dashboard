f90c2872beba4948ed04d09120910aa0e5c79bcf
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-19 18:50:21 +0200

    Update JobStats.js

--- src/stats/JobStats.js
+++ src/stats/JobStats.js
@@ -3,19 +3,27 @@ import axios from "../axios.js";
 
 const JobStats = (props) => {
 
+    var allJobs = props.allJobs
+    var activeJobs = props.activeJobs
+    var beginnerNum = props.beginnerNum
+    var intermediateNum = props.intermediateNum
+    var AdvancedNum = props.AdvancedNum
+    var expectedPriceAvg = props.expectedPriceAvg
+    var workingHoursAvg = props.workingHoursAvg
+
 
 return(
 
 <div className="job-stats">
 <h2>Jobs stats</h2>
 
-    <p>Num of all jobs: <b>xx</b></p>
-    <p>Num of active jobs: <b>xx</b></p>
-    <p>Beginner Level: <b>xx</b></p>
-    <p>intermediate Level: <b>xx</b></p>
-    <p>Advanced Level: <b>xx</b></p>
-    <p>Avg expected price: <b>xx</b></p>
-    <p>Avg working hours: <b>xx</b></p>
+    <p>Num of all jobs: <b>{allJobs}</b></p>
+    <p>Num of active jobs: <b>{activeJobs}</b></p>
+    <p>Beginner Level: <b>{beginnerNum}</b></p>
+    <p>intermediate Level: <b>{intermediateNum}</b></p>
+    <p>Advanced Level: <b>{AdvancedNum}</b></p>
+    <p>Avg expected price: <b>{expectedPriceAvg}</b></p>
+    <p>Avg working hours: <b>{workingHoursAvg}</b></p>
     
 </div>
 
