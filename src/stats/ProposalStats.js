25b500e15ffec0b200e61546c47be090dfe3109a
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-19 15:15:28 +0200

    Create ProposalStats.js

---
+++ src/stats/ProposalStats.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, createRef } from "react";
+import axios from "../axios.js";
+
+const ProposalStats = (props) => {
+
+    
+
+return(
+
+
+<div className="proposal-stats">
+
+<h2>Proposals stats</h2>
+
+    <p>number of proposals: <b>xx</b></p>
+    <p>number of opened proposals: <b>xx</b></p>
+    <p>Avg. price: <b>xx</b></p>
+    <p>Avg. expected duration: <b>xx</b></p>
+    
+</div>
+
+
+
+);
+
+}
+
+export default ProposalStats
