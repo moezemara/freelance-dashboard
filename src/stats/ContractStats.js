55abcd4ff865676fdeaecdc1613ce184bacd7a91
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-19 18:50:18 +0200

    Update ContractStats.js

--- src/stats/ContractStats.js
+++ src/stats/ContractStats.js
@@ -3,19 +3,19 @@ import axios from "../axios.js";
 
 const ContractStats = (props) => {
 
-    
+    var inInterview = props.inInterview
+    var active = props.active
+    var archieved = props.archieved
+    var finalPrice = props.finalPrice
 
 return(
 
-
 <div className="contract-stats">
-
 <h2>Contracts stats</h2>
-
-    <p>In-intervieiw contracts: <b>xx</b></p>
-    <p>Active contracts: <b>xx</b></p>
-    <p>Archieved contracts: <b>xx</b></p>
-    <p>Avg. Final price: <b>xx</b></p>
+    <p>In-intervieiw contracts: <b>{inInterview}</b></p>
+    <p>Active contracts: <b>{active}</b></p>
+    <p>Archieved contracts: <b>{archieved}</b></p>
+    <p>Avg. Final price: <b>{finalPrice}</b></p>
 
 </div>
 
