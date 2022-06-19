ed107f7c8b08f420aa3a31844d9e0ca9f74da6eb
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-20 01:53:51 +0200

    json  update + format

--- src/stats/SiteStats.js
+++ src/stats/SiteStats.js
@@ -6,62 +6,51 @@ import ContractStats from "./ContractStats.js";
 import JobStats from "./JobStats.js";
 
 const SiteStats = () => {
-    const [myData, setMyData] = useState({profile:{}});
+  const [myData, setMyData] = useState({ profile: {} });
 
-    useEffect(()=>{
-        axios.get(`stats`).then(res=>{
-            if(res.data.success===1){
-                setMyData(res.data.message);
-                console.log(res);
-            }
-            else{
-                axios.get(`stats`).then(res=>{
-                    if(res.data.success){
-                        setMyData(res.data.message);
-                        console.log(res);
-                    }
-                    else{
-                        console.log(res);
-                    }
-                });
-            }
-
+  useEffect(() => {
+    axios.get(`stats`).then((res) => {
+      if (res.data.success === 1) {
+        setMyData(res.data.message);
+        console.log(res);
+      } else {
+        axios.get(`stats`).then((res) => {
+          if (res.data.success) {
+            setMyData(res.data.message);
+            console.log(res);
+          } else {
+            console.log(res);
+          }
         });
-    },[]);
+      }
+    });
+  }, []);
 
-
-
-return(
-
+  return (
     <div className="site-stats">
+      <h1>Site Statistics</h1>
 
-<h1>Site Statistics</h1>
+      <table className="stats-table">
+        <tr>
+          <td>
+            <AccountStats stats={myData} />
+          </td>
+          <td>
+            <JobStats stats={myData} />
+          </td>
+        </tr>
 
+        <tr>
+          <td>
+            <ProposalStats stats={myData} />
+          </td>
+          <td>
+            <ContractStats stats={myData} />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
 
-<table className="stats-table">
-
-<tr>
-<td><AccountStats stats={myData.AccountStats} />
-</td>
-<td><JobStats stats={myData.JobStats} />
-</td>
-</tr>
-
-<tr>
-
-<td><ProposalStats stats={myData.ProposalStats} />
-</td>
-<td><ContractStats stats={myData.ContractStats} />
-</td>
-</tr>
-
-</table>
-
-
-
-</div>
-);
-
-}
-
-export default SiteStats
+export default SiteStats;
