071d056a830f82c003391f7de3abcd067e587ca3
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-19 18:50:24 +0200

    Update ProposalStats.js

--- src/stats/ProposalStats.js
+++ src/stats/ProposalStats.js
@@ -3,19 +3,19 @@ import axios from "../axios.js";
 
 const ProposalStats = (props) => {
 
-    
+    var proposalsNum = props.proposalsNum
+    var OpenedProposalNum = props.OpenedProposalNum
+    var priceAvg = props.priceAvg
+    var durationAvg = props.durationAvg
 
 return(
-
-
 <div className="proposal-stats">
 
 <h2>Proposals stats</h2>
-
-    <p>number of proposals: <b>xx</b></p>
-    <p>number of opened proposals: <b>xx</b></p>
-    <p>Avg. price: <b>xx</b></p>
-    <p>Avg. expected duration: <b>xx</b></p>
+    <p>number of proposals: <b>{proposalsNum}</b></p>
+    <p>number of opened proposals: <b>{OpenedProposalNum}</b></p>
+    <p>Avg. expected price: <b>{priceAvg}</b></p>
+    <p>Avg. expected duration: <b>{durationAvg}</b></p>
     
 </div>
 
