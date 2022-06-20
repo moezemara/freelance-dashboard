ba75949febb7c9365975be1c0a2897ac93096317
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-20 03:29:11 +0200

    Update ProposalStats.js

--- src/stats/ProposalStats.js
+++ src/stats/ProposalStats.js
@@ -1,6 +1,7 @@
 
 const ProposalStats = (props) => {
 
+
     var appliedNum = props.stats.num_proposals
     var activeNum = props.stats.num_active_proposals
     var archeivedNum = props.stats.num_archived_proposals
@@ -9,9 +10,9 @@ return(
 <div className="proposal-stats">
 
 <h2>Proposals stats</h2>
-    <p>number of applied proposals: <b>{appliedNum}</b></p>
-    <p>number of active proposals: <b>{activeNum}</b></p>
-    <p>number of archeived proposals: <b>{archeivedNum}</b></p>
+    <p>number of applied proposals:&ensp;<b>{appliedNum}</b></p>
+    <p>number of active proposals:&ensp;<b>{activeNum}</b></p>
+    <p>number of archeived proposals:&ensp;<b>{archeivedNum}</b></p>
         
 </div>
 
