654b47b95f3e32aec874ce2295452b16a3ae27ab
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-06-20 02:01:50 +0200

    Update JobStats.js

--- src/stats/JobStats.js
+++ src/stats/JobStats.js
@@ -1,18 +1,18 @@
 const JobStats = (props) => {
 
-    var allJobs = props.allJobs
-    var activeJobs = props.activeJobs
-    var entryNum = props.entryNum
-    var intermediateNum = props.intermediateNum
-    var AdvancedNum = props.AdvancedNum
-    var expectedPriceAvg = props.expectedPriceAvg
-    var workingHoursAvg = props.workingHoursAvg
+    var allJobs = props.num_jobs
+    var activeJobs = props.num_active_jobs
+    var entryNum = props.num_entry_jobs
+    var intermediateNum = props.num_intermediate_jobs
+    var AdvancedNum = props.num_advanced_jobs
+    var expectedPriceAvg = props.avg_expected_price_jobs
 
 
 return(
 
 <div className="job-stats">
 <h2>Jobs stats</h2>
+<br /><br />
 
     <p>Num of all jobs: <b>{allJobs}</b></p>
     <p>Num of active jobs: <b>{activeJobs}</b></p>
@@ -20,7 +20,6 @@ return(
     <p>intermediate Level: <b>{intermediateNum}</b></p>
     <p>Advanced Level: <b>{AdvancedNum}</b></p>
     <p>Avg expected price: <b>{expectedPriceAvg}</b></p>
-    <p>Avg working hours: <b>{workingHoursAvg}</b></p>
     <br /><br />
 </div>
 
