36eaa05cd168d797bcb612ffd4401c415219a3a0
Kareem Elozeiri <k.elozeiri@gmail.com>
2022-07-02 20:42:35 +0000

    few changes

--- src/Chat.js
+++ src/Chat.js
@@ -5,6 +5,7 @@ import axios from "./axios.js";
 import { useEffect } from "react";
 import socket from "./socket.js"
 import accountCheck from "./accountCheck";
+import arrow from "./src-images/service-logo.png";
 
 const Chat = ()=>{
     const cookies = new Cookies();
@@ -21,6 +22,10 @@ const Chat = ()=>{
     <div className="chat">
         {(accountType==='F') && <FreelancerNavbar profile_id={profile_id}/>}
         {(accountType==='C') && <ClientNavbar profile_id={profile_id}/>}
+        <div style={{"display":"flex","width":"100%","height":"50"}}>
+            <input type="text" placeholder="Type something"/>
+            <button><img src={arrow} height="35px"/></button>
+        </div>
     </div>
     )
 }
