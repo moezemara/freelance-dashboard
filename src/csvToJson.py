9ede689965b2917c0cf580203466999a951262ea
Kareem Elozeiri <k.elozeiri@gmail.com>
2022-07-11 17:05:44 +0000

    refactoring project

--- src/csvToJson.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r'countries.csv')
-df.to_json(r'countries.json')
--- src/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = new io("https://api.plebits.com/", {
-  autoConnect: false,
-  withCredentials: true
-  //transports: ['websocket']
-});
-
-export default socket;
