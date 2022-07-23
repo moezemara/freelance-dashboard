fa74af7b51d2d750c163f8b7be97b9f6710df8e7
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-07-23 20:35:47 +0200

    account settings init

--- src/accounts/AccountSettings.js
+++ src/accounts/AccountSettings.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Cookies from "universal-cookie";
+
+import ClientNavbar from "../navbars/ClientNavbar";
+import FreelancerNavbar from "../navbars/FreelancerNavbar";
+
+
 
 
 const AccountSettings = ()=>{
+
+    const [accountType, setAccountType] = useState();
+    const { profile_id } = useParams();
+    const cookies = new Cookies();
+
 
     const [fname,setFname] = useState('');
     const [lname,setLname] = useState('');
@@ -14,11 +27,28 @@ const AccountSettings = ()=>{
     const [sex, setSex] = useState('');
     
 
+    useEffect(() => {
+        if (accountType !== "F" || accountType !== "C") {
+          setAccountType(cookies.getAll().type);
+        }
+      }, [accountType]);
+
 return(
 <div>
 
+{accountType === "F" && <FreelancerNavbar profile_id={profile_id} />}
+      {accountType === "C" && <ClientNavbar profile_id={profile_id} />}
 
 
+      <div className="accountsettings">
+      <h1>Account settings</h1>
+        
+
+        
+
+
+
+      </div>
 
 </div>
 );
