cec00fad7d0abe572df728a0f5c48ffca6e438f8
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-08-14 04:06:15 +0200

    payment client

--- src/payement/PaymentPage.js
+++ src/payement/PaymentPage.js
@@ -10,31 +10,50 @@ const PaymentPage = () => {
 
     const recaptchaRef = createRef();
     const [availableBalance, setAvailableBalance] = useState(0);
-
-
+    const [paymentOption,setPaymentOption] = useState('');
+    const [paymentAmount, setPaymentAmount] = useState('')
     const cookies = new Cookies();
-    const accountType = cookies.getAll().type;
+    const accountType = "F"; //cookies.getAll().type;
 
      //account check
      useEffect(()=>{
         accountCheck();
       },[]);
 
+
+    const proceedPayment = () => {
+        const data = {
+            'option':paymentOption,
+            'amount':paymentAmount
+        }
+
+        axios.post('pay/',data,{ withCredentials: true}).then(res=>{
+            if(res.data.success===1){
+              window.location = '/pay/';
+            }},[]);    
+    }
+
     return (
         <div>
             {(accountType=="F") && <FreelancerNavbar/>}
             {(accountType=="C") && <ClientNavbar/>}
+            
+            
             <div className="signup">
                 <h2>Payement</h2>
                 <form>
                     <p><b>Available balance:</b> {availableBalance} $</p>
-                    <select>
-                        <option value="" disabled selected hidden>Select Payement Method</option>                        
+                    <select value={paymentOption} onInput={e=>setPaymentOption(e.target.value)}>
+                        <option value="" disabled selected hidden>Select Payement Method</option> 
+                        <option value="paypal" >Paypal</option>                        
+                       
                     </select>
-                    <input type="text"/>
-                    <button type="button">Sign Up</button>
+                    <input type="text" value ={paymentAmount} onInput={e=>setPaymentAmount(e.target.value)} placeholder="amount" />
+                    <button type="button" onClick={proceedPayment}>Pay</button>
                 </form>
             </div>
+
+            
         </div>
     );
 }
