bc5380313ca93346de737e9f416e99d5196a9e75
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-09-13 13:10:06 +0200

    Update PaymentPayPal.js

--- src/payement/PaymentPayPal.js
+++ src/payement/PaymentPayPal.js
@@ -3,95 +3,92 @@ import ClientNavbar from "../navbars/ClientNavbar.js";
 
 import { useEffect } from "react";
 import {
-    PayPalScriptProvider,
-    PayPalButtons,
-    usePayPalScriptReducer
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
 } from "@paypal/react-paypal-js";
 
-
 const PaymentPayPal = () => {
+  const amount = "2";
+  const currency = "USD";
+  const style = { layout: "vertical" };
 
+  const accountType = "F";
 
-// This values are the props in the UI
-const amount = "2";
-const currency = "USD";
-const style = {"layout":"vertical"};
-
-const accountType= 'F';
-
-// Custom component to wrap the PayPalButtons and handle currency changes
-const ButtonWrapper = ({ currency, showSpinner }) => {
-    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
-    // This is the main reason to wrap the PayPalButtons in a new component
+  const ButtonWrapper = ({ currency, showSpinner }) => {
     const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
 
     useEffect(() => {
-        dispatch({
-            type: "resetOptions",
-            value: {
-                ...options,
-                currency: currency,
-            },
-        });
+      dispatch({
+        type: "resetOptions",
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
     }, [currency, showSpinner]);
 
-
-    return (<>
-            { (showSpinner && isPending) && <div className="spinner" /> }
-            <PayPalButtons
-                style={style}
-                disabled={false}
-                forceReRender={[amount, currency, style]}
-                fundingSource={undefined}
-                createOrder={(data, actions) => {
-                    return actions.order
-                        .create({
-                            purchase_units: [
-                                {
-                                    amount: {
-                                        currency_code: currency,
-                                        value: amount,
-                                    },
-                                },
-                            ],
-                        })
-                        .then((orderId) => {
-                            // Your code here after create the order
-                            return orderId;
-                        });
-                }}
-                onApprove={function (data, actions) {
-                    return actions.order.capture().then(function () {
-                        // Your code here after capture the order
-                    });
-                }}
-            />
-        </>
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function () {
+              // Your code here after capture the order
+            });
+          }}
+        />
+      </>
     );
-}
+  };
 
-return (
+  return (
     <div>
+      {accountType == "F" && <FreelancerNavbar />}
+      {accountType == "C" && <ClientNavbar />}
 
-         {(accountType=="F") && <FreelancerNavbar/>}
-            {(accountType=="C") && <ClientNavbar/>}
-    
-    <div style={{ maxWidth: "750px", minHeight: "200px" }}>
+      <div style={{ minWidth: "750px", minHeight: "200px" ,
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    /* bring your own prefixes */
+    transform: "translate(-50%, -50%)"
+    }}>
+
+
         <PayPalScriptProvider
-            options={{
-                "client-id": "test",
-                components: "buttons",
-                currency: "USD"
-            }}
+          options={{
+            "client-id": "test",
+            components: "buttons",
+            currency: "USD",
+          }}
         >
-            <ButtonWrapper
-                currency={currency}
-                showSpinner={false}
-            />
+          <ButtonWrapper currency={currency} showSpinner={false} />
         </PayPalScriptProvider>
+      </div>
     </div>
-    </div>
-);
-        }
+  );
+};
 
 export default PaymentPayPal;
