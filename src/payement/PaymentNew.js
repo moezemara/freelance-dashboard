5c8c0f4b7e9aefb547d632853320e7f132d97405
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-09-13 12:51:32 +0200

    update 2

--- src/payement/PaymentNew.js
+++ src/payement/PaymentNew.js
@@ -6,117 +6,95 @@ import FreelancerNavbar from "../navbars/FreelancerNavbar.js";
 import ClientNavbar from "../navbars/ClientNavbar.js";
 import accountCheck from "../shared/accountCheck.js";
 
+
+import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+
+
 const PaymentNew = () => {
 
     const accountType = "F"; //cookies.getAll().type;
 
     
 
+
+    const [show, setShow] = useState(false)
+    const [success, setSuccess] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const [orderId, setOrderId] = useState(false)
+    
+    const createOrder = (data, actions) => {
+      return actions.order.create({
+        purchase_units: [
+          {
+            description: 'This is the Book Worth 100$',
+            amount: {
+              currency_code: 'USD',
+              value:100
+            },
+          },  
+        ],
+        application_context: {
+          shipping_preference:'NO_SHIPPING'
+        }
+      })
+        .then((orderID) => {
+          setOrderId(orderID)
+          return orderID
+      })
+    }
+   
+    const onApprove = (data,actions) => {
+      return actions.order.capture().then(function (details) {
+        const { payer } = details
+        setSuccess(true)
+      })
+    }
+  
+    const onError = (data, actions) => {
+      setErrorMessage("An error occured with your payment")
+    }  
+
+
+
+<PayPalScriptProvider
+        options={{
+          "client-id":
+            "###replacethiswithyourownclientid###"
+        }}
+      >
+      <h1>Simple Book Worth 100$</h1>
+      <span>100$</span>
+      <button onClick={() => setShow(true) } type='submit'>
+        Buy Now
+      </button>
+
+      {show ? (
+          <PayPalButtons style={{ layout: 'vertical' }} createOrder={createOrder}
+            onApprove={onApprove} onError={onError}/>
+        ) : null}
+        
+        {success ? (
+          <h1>Your Payment has been done successfully please check email</h1>
+        ):<h2>payment is pending</h2>}
+      
+    </PayPalScriptProvider>
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div>
             {(accountType=="F") && <FreelancerNavbar/>}
             {(accountType=="C") && <ClientNavbar/>}
             
-
-
-
-
-
-
- <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://www.paypalobjects.com/webstatic/en_US/developer/docs/css/cardfields.css"
-    />
-    <script
-      src="https://www.paypal.com/sdk/js?components=buttons,hosted-fields&client-id=<%= clientId %>"
-      data-client-token="<%= clientToken %>"
-    ></script>
-    <div id="paypal-button-container" class="paypal-button-container"></div>
-    <div class="card_container">
-      <form id="card-form">
-        <label for="card-number">Card Number</label>
-        <div id="card-number" class="card_field"></div>
-        <div >
-          <div>
-            <label for="expiration-date">Expiration Date</label>
-            <div id="expiration-date" class="card_field"></div>
-          </div>
-          <div >
-            <label for="cvv">CVV</label>
-            <div id="cvv" class="card_field"></div>
-          </div>
-        </div>
-        <label for="card-holder-name">Name on Card</label>
-        <input
-          type="text"
-          id="card-holder-name"
-          name="card-holder-name"
-          autocomplete="off"
-          placeholder="card holder name"
-        />
-        <div>
-          <label for="card-billing-address-street">Billing Address</label>
-          <input
-            type="text"
-            id="card-billing-address-street"
-            name="card-billing-address-street"
-            autocomplete="off"
-            placeholder="street address"
-          />
-        </div>
-        <div>
-          <label for="card-billing-address-unit">&nbsp;</label>
-          <input
-            type="text"
-            id="card-billing-address-unit"
-            name="card-billing-address-unit"
-            autocomplete="off"
-            placeholder="unit"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="card-billing-address-city"
-            name="card-billing-address-city"
-            autocomplete="off"
-            placeholder="city"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="card-billing-address-state"
-            name="card-billing-address-state"
-            autocomplete="off"
-            placeholder="state"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="card-billing-address-zip"
-            name="card-billing-address-zip"
-            autocomplete="off"
-            placeholder="zip / postal code"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="card-billing-address-country"
-            name="card-billing-address-country"
-            autocomplete="off"
-            placeholder="country code"
-          />
-        </div>
-        <br /><br />
-        <button value="submit" id="submit" class="btn">Pay</button>
-      </form>
-    </div>
-
-
 
 
 
